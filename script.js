c7762a6d737dd5345f4ec9ff9245810b32d12d18
Farnerud <hperea.alfa@gmail.com>
2021-05-19 20:02:20 -0500

    Add stroke dasharray attributes by JS and set the dashoffset.

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-
 // select the dom elements
 const durationInput = document.querySelector("#duration");
 const startButton = document.querySelector("#start");
 const pauseButton = document.querySelector("#pause");
+const circle = document.querySelector("circle");
+
+// Calculate circle´s radius
+const perimeter = Math.floor(circle.getAttribute("r") * 2 * Math.PI);
+// Add the perimeter value to the dasharray circle property
+circle.setAttribute("stroke-dasharray", perimeter);
+
+let currentOffset = 0;
 
 // create a new instance of the timer
 const timer = new Timer(durationInput, startButton, pauseButton, {
@@ -10,7 +17,8 @@ const timer = new Timer(durationInput, startButton, pauseButton, {
     console.log("Timer has started");
   },
   onTick() {
-    console.log("Timer just ticked down");
+    circle.setAttribute("stroke-dashoffset", currentOffset);
+    currentOffset -= 20;
   },
   onComplete() {
     console.log("Timer is completed");
